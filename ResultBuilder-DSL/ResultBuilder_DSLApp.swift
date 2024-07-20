7c9c391b3517bf94a1703483f6dd9439398bc64b
NhatLe <lenhathk@gmail.com>
2024-07-20 09:27:29 +0700

    Initial Commit

---
+++ ResultBuilder-DSL/ResultBuilder_DSLApp.swift
@@ -0,0 +1,32 @@
+//
+//  ResultBuilder_DSLApp.swift
+//  ResultBuilder-DSL
+//
+//  Created by Nhat Le on 20/7/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ResultBuilder_DSLApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ResultBuilder-DSLUITests/ResultBuilder_DSLUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ResultBuilder_DSLUITestsLaunchTests.swift
+//  ResultBuilder-DSLUITests
+//
+//  Created by Nhat Le on 20/7/24.
+//
+
+import XCTest
+
+final class ResultBuilder_DSLUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
